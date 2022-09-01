d01bbda5a75516b38a1952351e4d39be63a4d7d2
Vladimir Turbabin <vladimirturbabin@gmail.com>
2022-09-01 17:43:04 +0300

    Make home work number_3, and make commit

--- Program.cs
+++ Program.cs
@@ -1,67 +1,89 @@
-﻿
-//Задача 10
-
-Console.WriteLine("Введите трехзначное число");
+﻿/*Задача №19
+Console.Write("Введите число: ");
 string number = Console.ReadLine();
 
-Console.WriteLine(number[1]);
-
-//Задача 13
-
-Console.WriteLine("Введите число");
-string number = Console.ReadLine();
-
-if (number.Length > 2)
+void CheckingNumber(string number)
 {
-    Console.WriteLine(number[2]);
-}
-else
-{
-    Console.WriteLine("третьей цифры нет");
+    if (number[0] == number[4] || number[1] == number[3])
+    {
+        Console.WriteLine($"Ваше число: {number} - палиндром.");
+    }
+    else Console.WriteLine($"Ваше число: {number} - НЕ палиндром.");
 }
 
-//Задача 13
-
-
-
-Console.WriteLine("Введите число обозначающее день недели");
-Console.WriteLine("1 - Понедельник");
-Console.WriteLine("2 - Вторник");
-Console.WriteLine("3 - Среда");
-Console.WriteLine("4 - Четверг");
-Console.WriteLine("5 - Пятница");
-Console.WriteLine("6 - Суббота");
-Console.WriteLine("7 - Воскресение");
-Console.WriteLine("8 - Выход");
-
-int userInput = Convert.ToInt32(Console.ReadLine());
-
-switch (userInput)
+if (number.Length == 5)
 {
-    case 1:
-        Console.WriteLine("Это не выходной");
-        break;
-    case 2:
-        Console.WriteLine("Это не выходной");
-        break;
-    case 3:
-        Console.WriteLine("Это не выходной");
-        break;
-    case 4:
-        Console.WriteLine("Это не выходной");
-        break;
-    case 5:
-        Console.WriteLine("Это не выходной");
-        break;
-    case 6:
-        Console.WriteLine("Это выходной");
-        break;
-    case 7:
-        Console.WriteLine("Это выходной");
-        break;
-    case 8:
-        break;
+
+    CheckingNumber(number);
 }
-Console.WriteLine("Нажмите ENTER для выхода");
-Console.ReadKey();
-Console.Clear();
+else Console.WriteLine($"Введи правильное число");*/
+
+/* Задача №21
+
+System.Console.WriteLine("Введите координату x первой точки");
+bool isNumberX_1 = int.TryParse(Console.ReadLine(), out int x1);
+
+System.Console.WriteLine("Введите координату y первой точки");
+bool isNumberY_1 = int.TryParse(Console.ReadLine(), out int y1);
+
+System.Console.WriteLine("Введите координату x второй точки");
+bool isNumberX_2 = int.TryParse(Console.ReadLine(), out int x2);
+
+System.Console.WriteLine("Введите координату y второй точки");
+bool isNumberY_2 = int.TryParse(Console.ReadLine(), out int y2);
+
+System.Console.WriteLine("Введите координату z третьей точки");
+bool isNumberZ_1 = int.TryParse(Console.ReadLine(), out int z1);
+
+System.Console.WriteLine("Введите координату z третьей точки");
+bool isNumberZ_2 = int.TryParse(Console.ReadLine(), out int z2);
+
+if(!isNumberX_1 || !isNumberX_2 || !isNumberY_1 || !isNumberY_2 || !isNumberZ_1 || !isNumberZ_2 )
+{
+    Console.WriteLine("Числа введены неправильно ");
+}
+else{
+System.Console.Write($"Расстояние {GetLenght(x1,y1,x2,y2,z1,z2)}");
+}
+double GetLenght(int x1,int y1,int x2,int y2,int z1,int z2)
+{
+    return Math.Sqrt(Math.Pow((x2-x1),2)+Math.Pow((y2-y1),2)+Math.Pow((z2-z1),2));
+}
+
+/*
+
+Задача №23
+
+Console.WriteLine("Введите положительное N");
+bool isNumberN = int.TryParse(Console.ReadLine(), out int N);
+
+if (!isNumberN)
+{
+    Console.WriteLine("Не число");
+    return;
+}
+
+PrintSqares (GetSqares(N));
+int[] GetSqares(int n)
+{
+    int i = 1;
+    int[] results = new int[n];
+    while (i <= n)
+    {
+        results[i-1] = (int) Math.Pow (i,3);
+        i++;
+    }
+    return results;
+}
+
+void PrintSqares(int[] sqares) 
+{
+    int i = 0;
+    int len = sqares.Length;
+    while (i < len)
+    {
+        Console.WriteLine(sqares[i]);
+        i++;
+    }
+}
+/*
